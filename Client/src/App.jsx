22d72ef1df01de180301cf0d7211d27cb6e2c796
tomas bombau <tomas.bombau@gmail.com>
2023-12-06 18:00:17 -0300

    Maquetacion básica sin funcionalidades de LOGIN, NAVBAR y SIDEBAR

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,8 @@
 // Vistas:
 import Landing from "./views/Landing.jsx";
 import Home from "./views/Home.jsx";
-// Componentes:
-import NavBar from "./components/NavBar.jsx";
 // hooks, routers, reducers:
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 // Variables de entorno:
 import getParamsEnv from "./functions/getParamsEnv.js";
 
@@ -12,11 +10,9 @@ import getParamsEnv from "./functions/getParamsEnv.js";
 const { ROOT, HOME } = getParamsEnv();
 
 const App = () => {
-  const location = useLocation()
 
   return (
     <div>
-      {location.pathname !== "/" ?  <NavBar /> : null}
       <Routes>
         <Route path={ROOT} element={<Landing />} />
         <Route path={HOME} element={<Home />} />
--- Client/src/views/Home.jsx
+++ Client/src/views/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import SideBar from "../components/SideBar";
+import NavBar from "../components/NavBar";
 
 const Home = () => {
   return (
     <div>
+      <NavBar />
       <SideBar />
     </div>
   );
