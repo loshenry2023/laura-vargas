50f7527100466f0cb1caaf7b82ec2c17ad997bfb
mactrias <73613066+mactrias@users.noreply.github.com>
2023-12-09 20:13:00 -0300

    showlogs

--- Server/src/controllers/user/getAllUsers.js
+++ Server/src/controllers/user/getAllUsers.js
@@ -2,14 +2,13 @@
 const axios = require("axios");
 const { User, Specialty, Branch } = require("../../DB_connection");
 const { Op } = require("sequelize");
-const showLog = require("../../functions/showLog");
 
 const getAllUsers = async (
   nameOrLastName = "",
   attribute = "createdAt",
   order = "desc",
   page = 0,
-  size = 10,
+  size = 4,
   branch = "",
   specialty = "",
   role = "",
@@ -17,45 +16,48 @@ const getAllUsers = async (
   createDateStart = ""
 ) => {
   const { count, rows } = await User.findAndCountAll({
-    include: [
-      {
-        model: Specialty,
-        where: { specialtyName: { [Op.iLike]: `%${specialty}%` } },
-        as: "Specialties",
-        through: { attributes: [] },
-        attributes: ["id", "specialtyName"],
-      },
-      {
-        model: Branch,
-        where: { branchName: { [Op.iLike]: `%${branch}%` } },
-        attributes: ["id", "branchName"],
-      },
-    ],
-    attributes: ["id", "name", "lastName", "userName", "role", "createdAt"],
-    where: {
-      [Op.or]: [
-        //filtro por nombres
-        { name: { [Op.iLike]: `%${nameOrLastName}%` } },
-        { lastName: { [Op.iLike]: `%${nameOrLastName}%` } },
+  try {
+    const { count, rows } = await User.findAndCountAll({
+      include: [
+        {
+          model: Specialty,
+          where: { specialtyName: { [Op.iLike]: `%${specialty}%` } },
+          as: "Specialties",
+          through: { attributes: [] },
+          attributes: ["id", "specialtyName"],
+        },
+        {
+          model: Branch,
+          where: { branchName: { [Op.iLike]: `%${branch}%` } },
+          attributes: ["id", "branchName"],
+        },
       ],
-      role: role ? role : [`user`, `superAdmin`, `admin`],
+      attributes: ["id", "name", "lastName", "userName", "role", "createdAt"],
+      where: {
+        [Op.or]: [
+          //filtro por nombres
+          { name: { [Op.iLike]: `%${nameOrLastName}%` } },
+          { lastName: { [Op.iLike]: `%${nameOrLastName}%` } },
+        ],
+        role: role ? role : [`user`, `superAdmin`, `admin`],
 
-      createdAt: {
-        //para la fecha de creación
-        [Op.gte]: createDateStart || "1900-01-01",
-        [Op.lte]: createDateEnd || new Date(),
+        createdAt: {
+          //para la fecha de creación
+          [Op.gte]: createDateStart || "1900-01-01",
+          [Op.lte]: createDateEnd || new Date(),
+        },
       },
-    },
 
-    order: [[attribute, order]],
-    limit: size,
-    offset: size * page,
-  });
-  return {
-    count,
-    rows,
-  };
+      order: [[attribute, order]],
+      limit: size,
+      offset: size * page,
+    });
+    return {
+      count,
+      rows,
+    };
+  } catch (err) {
+    return { message: err.message };
+  }
 };
-
-showLog("getAllUsers cargados");
 module.exports = getAllUsers;
