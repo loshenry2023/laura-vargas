862d48947a5529de591d08f4929c3e0218dc008d
tomas bombau <tomas.bombau@gmail.com>
2023-12-09 21:22:22 -0300

    FILTROS realizados en view perfiles

--- Client/src/functions/getParamsEnv.js
+++ Client/src/functions/getParamsEnv.js
@@ -4,6 +4,7 @@ export default function getParamsEnv() {
     const LOGIN = import.meta.env.VITE_ROOT || '/login';
     const HOME = import.meta.env.VITE_HOME || '/home';
     const USERPROFILES = import.meta.env.VITE_ROOT || '/userProfiles';
+    const USERDETAIL = import.meta.env.VITE_ROOT || '/detail/:id';
 
     return {ROOT, HOME, LOGIN, USERPROFILES}
 }
--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -1,9 +1,66 @@
-import {GET_USER, ASD, ASDD} from './actionsTypes';
+import {GET_USER, GET_USERS, GET_USER_ID, GET_BRANCHES, GET_SPECIALTIES} from './actionsTypes';
+import axios from 'axios'
 
 
 export const getUser = (userData) => {
     return {
       type: GET_USER,
       payload: userData,
+    };
+  };
+
+  export const getBranches = () => {
+    return async function (dispatch) {
+      try {
+        const response = await axios.get("http://localhost:3001/laura-vargas/branches");
+        return dispatch({
+          type: GET_BRANCHES,
+          payload: response.data,
+        });
+      } catch (error) {
+        throw Error(error.message);
+      }
+    }
+  };
+
+  export const getSpecialties = () => {
+    return async function (dispatch) {
+      try {
+        const response = await axios.get("http://localhost:3001/laura-vargas/specialties");
+        return dispatch({
+          type: GET_SPECIALTIES,
+          payload: response.data,
+        });
+      } catch (error) {
+        throw Error(error.message);
+      }
+    }
+  };
+
+  export const getUsers = () => {
+    return async function (dispatch) {
+      try {
+        const response = await axios.get("http://localhost:3001/laura-vargas/users");
+        return dispatch({
+          type: GET_USERS,
+          payload: response.data,
+        });
+      } catch (error) {
+        throw Error(error.message);
+      }
+    }
+  }
+
+  export const getUserId = (id) => {
+    return async function (dispatch) {
+      try {
+        const response = await axios.get(`http://localhost:3001/laura-vargas/userdata/${id}`);
+        return dispatch({
+          type: GET_USER_ID,
+          payload: response.data,
+        });
+      } catch (error) {
+        throw Error(error.message);
+      }
     };
   };
