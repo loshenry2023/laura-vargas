e8e814bd8306f3c28169ff4c6ee61234bf0469e6
fsuppanieto <fsuppanieto@hotmail.com>
2023-12-15 18:49:10 -0300

    variables de entorno

--- Client/src/firebase/firebaseConfig.js
+++ Client/src/firebase/firebaseConfig.js
@@ -2,17 +2,26 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import getParamsEnv from "../functions/getParamsEnv";
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} = getParamsEnv();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAgacLbFO0UUII7SX_COA6tTgDCJh5MyIU",
-  authDomain: "laura-vargas-7f3ad.firebaseapp.com",
-  projectId: "laura-vargas-7f3ad",
-  storageBucket: "laura-vargas-7f3ad.appspot.com",
-  messagingSenderId: "658315611360",
-  appId: "1:658315611360:web:56f139b3d2af20fb13d485"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app
+export default app;
--- Client/src/functions/getParamsEnv.js
+++ Client/src/functions/getParamsEnv.js
@@ -1,11 +1,46 @@
 //! Unico lugar donde obtengo las variables de entorno.
 export default function getParamsEnv() {
-    const ROOT = import.meta.env.VITE_ROOT || '/';
-    const LOGIN = import.meta.env.VITE_ROOT || '/login';
-    const HOME = import.meta.env.VITE_HOME || '/home';
-    const USERPROFILES = import.meta.env.VITE_ROOT || '/userProfiles';
-    const USERDETAIL = import.meta.env.VITE_ROOT || '/detail/:id';
-    const API_URL_BASE = import.meta.env.VITE_API_URL_BASE || 'http://localhost:3001/laura-vargas';
+  const ROOT = import.meta.env.VITE_ROOT || "/";
+  const LOGIN = import.meta.env.VITE_ROOT || "/login";
+  const HOME = import.meta.env.VITE_HOME || "/home";
+  const USERPROFILES = import.meta.env.VITE_ROOT || "/userProfiles";
+  const USERDETAIL = import.meta.env.VITE_ROOT || "/detail/:id";
+  const API_URL_BASE =
+    import.meta.env.VITE_API_URL_BASE || "http://localhost:3001/laura-vargas";
 
-    return {ROOT, HOME, LOGIN, USERPROFILES, API_URL_BASE}
+  const CLOUD_NAME = import.meta.env.VITE_APP_CLOUD_NAME || "doqyrz0sg";
+  const UPLOAD_PRESET = import.meta.env.VITE_APP_UPLOAD_PRESET || "gcx7ffyb";
+
+  const FIREBASE_API_KEY =
+    import.meta.env.VITE_APP_FIREBASE_API_KEY ||
+    "AIzaSyAgacLbFO0UUII7SX_COA6tTgDCJh5MyIU";
+  const FIREBASE_AUTH_DOMAIN =
+    import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN ||
+    "laura-vargas-7f3ad.firebaseapp.com";
+  const FIREBASE_PROJECT_ID =
+    import.meta.env.VITE_APP_FIREBASE_PROJECT_ID || "laura-vargas-7f3ad";
+  const FIREBASE_STORAGE_BUCKET =
+    import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET ||
+    "laura-vargas-7f3ad.appspot.com";
+  const FIREBASE_MESSAGING_SENDER_ID =
+    import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID || "658315611360";
+  const FIREBASE_APP_ID =
+    import.meta.env.VITE_APP_FIREBASE_APP_ID ||
+    "1:658315611360:web:56f139b3d2af20fb13d485";
+
+  return {
+    ROOT,
+    HOME,
+    LOGIN,
+    USERPROFILES,
+    API_URL_BASE,
+    CLOUD_NAME,
+    UPLOAD_PRESET,
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+  };
 }
