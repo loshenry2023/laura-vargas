ac82ac6aab6228f56cd3dd214c45e2ef74bf597e
PauloDamianVinci <paulodamianvinci@gmail.com>
2023-12-16 14:45:43 -0300

    fix back

--- Server/index.js
+++ Server/index.js
@@ -7,23 +7,18 @@ const createBasicData = require("./src/functions/createBasicData");
 let conSegura = "";
 SECURE ? (conSegura = "SECURE") : (conSegura = "NOT SECURE");
 
-conn
-  .authenticate()
-  .then(() => {
-    // La base de datos existe:
-    return conn.sync({ alter: true });
-  })
-  .then(() => {
-    // Antes de hacer el listen me encargo de verificar que existan los registros mínimos
-    // de trabajo en las tablas:
-    setTimeout(() => {
-      createBasicData();
-    }, 5000); // 5000 milisegundos = 5 segundos
+(async () => {
+  try {
+    await conn.authenticate();
+    await conn.sync({ alter: true });
+
+    // Verificar que existan los registros mínimos antes de iniciar el servidor:
+    await createBasicData();
 
     server.listen(PORT, () => {
       showLog(`Server running into ${PORT} Port. DB Connection: ${conSegura}`);
     });
-  })
-  .catch((err) => {
+  } catch (err) {
     showLog("Error connecting to the database (was it created?). Please check and restart");
-  });
+  }
+})();
