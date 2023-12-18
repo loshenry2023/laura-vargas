18d810c3ec022b0be7f607f10312ea54064cfb2b
mactrias <73613066+mactrias@users.noreply.github.com>
2023-12-18 19:46:58 -0300

    tablas productos
    dos tablas subidas

---
+++ Server/src/models/PriceHistory.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define(
+    "PriceHistory",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      codigo_producto: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      precio: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      fecha_modificacion: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
